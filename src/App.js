11c89c85fefb2b7a0d2c684d6495ec10e9801f66
Lninn <thl384458870@outlook.com>
2022-06-15 22:57:23 +0800

    add  react-tabs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import './App.css';
+
+function Foo() {
+  useEffect(() => {
+    console.log('Foo mounted');
+  }, []);
+
+  return <div>Foo</div>;
+}
+
+function Bar() {
+  useEffect(() => {
+    console.log('Bar mounted');
+  }, []);
+
+  return <div>Bar</div>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tabs forceRenderTabPanel>
+        <TabList>
+          <Tab>Tab 1</Tab>
+          <Tab>Tab 2</Tab>
+        </TabList>
+        <TabPanel>
+          <p>This is tab 1</p>
+          <Foo />
+        </TabPanel>
+        <TabPanel>
+          <p>This is tab 2</p>
+          <Bar />
+        </TabPanel>
+      </Tabs>
     </div>
   );
 }
