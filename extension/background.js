e89f5d46a958b5459a7be4af14a3a55801e8bce5
Lninn <thl384458870@outlook.com>
2022-07-31 15:30:13 +0800

    content script

--- extension/background.js
+++ extension/background.js
@@ -22,6 +22,28 @@ const main = () => {
       menuItemId,
       pageUrl,
       selectionText,
+    }, chrome)
+
+    // chrome.tabs.create({
+    //   url: 'popup.html',
+    // })
+
+    chrome.windows.create({
+      url: chrome.runtime.getURL('popup.html'),
+      type: 'popup'
+    })
+
+    chrome.tabs.query({}, (result) => {
+      console.log('Tabs query ', result)
+    })
+  })
+
+  chrome.runtime.onConnect.addListener((port) => {
+    console.log('Connected... ', port)
+
+    port.onMessage.addListener((msg) => {
+      console.log('message recived ' + msg);
+      port.postMessage('Hi Popup.js')
     })
   })
 }
--- extension/popup.js
+++ extension/popup.js
@@ -1,1 +1,10 @@
 console.log('123')
+
+const port = chrome.runtime.connect({
+  name: 'foo'
+})
+
+port.postMessage('Hi background.js')
+port.onMessage.addListener((msg) => {
+  console.log('Message recieved ' + msg)
+})
