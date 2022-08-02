3aca1b30baf1d09283ae427d0f7ca905bb75eece
Lninn <thl384458870@outlook.com>
2022-08-03 00:39:41 +0800

    update

--- src/background.js
+++ src/background.js
@@ -1,3 +1,5 @@
+import { md5 } from './utils'
+
 const main = () => {
   const commandId = 'Translate'
 
@@ -8,20 +10,28 @@ const main = () => {
   })
 
   // 通信
-  let LATEST_MESSAGE = ''
+  let LATEST_PAYLOAD = null
 
-  chrome.runtime.onMessage.addListener((msg) => {
-    console.log('bg received msg ', msg)
-    LATEST_MESSAGE = msg
+  chrome.runtime.onMessage.addListener((payload) => {
+    LATEST_PAYLOAD = payload
   })
 
   chrome.contextMenus.onClicked.addListener((evt) => {
     const { menuItemId } = evt
 
+    if (!LATEST_PAYLOAD) return
+
     if (menuItemId === 'Translate') {
-      fetch(LATEST_MESSAGE)
+      const url = createTranslateUrl(LATEST_PAYLOAD.q)
+
+      fetch(url)
         .then(res => res.json())
         .then(res => {
+          if (res.error_code === '54001') {
+            console.error('ERROR ', res.error_msg)
+            return
+          }
+
           const { trans_result } = res
           const [{ src, dst }] = trans_result
 
@@ -57,5 +67,50 @@ const notifications = (msg) => {
   chrome.notifications.create(notificationPayload)
 }
 
+const BAIDU_URL = 'https://fanyi-api.baidu.com/api/trans/vip/translate'
+const APP_ID = '20220801001290121'
+const KEY = 'yDr87UOd8xxpPYC0JjES'
+
+const createTranslateUrl = (q) => {
+  const url = BAIDU_URL
+
+  const salt = (new Date).getTime()
+  const sign = toSign(q, salt)
+
+  const params = {
+    q,
+    from: 'en',
+    to: 'zh',
+    appid: APP_ID,
+    salt,
+    sign,
+  }
+
+  const finalUrl = append(url, params)
+  
+  return finalUrl
+}
+
+const toSign = (q, salt) => {
+  const s = `${APP_ID}${q}${salt}${KEY}`
+
+  return md5(s)
+}
+
+const append = (url, params) => {
+  const qIdx = url.indexOf('?')
+  if (qIdx === -1) {
+    url = `${url}?`
+  }
+
+  return Object.keys(params).reduce((accu, next, idx) => {
+    const value = params[next]
+    const preFix = idx === 0 ? '' : '&'
+
+    accu += `${preFix}${next}=${value}`
+
+    return accu
+  }, url)
+}
 
 // https://developer.chrome.com/docs/extensions/reference/action/
