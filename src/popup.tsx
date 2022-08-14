2951d4184a2a7dfdbd4a1e843ede5ff843f9c915
Lninn <thl384458870@outlook.com>
2022-08-14 11:15:23 +0800

    init

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,8 +1,84 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import './popup.less'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom/client'
+import { queryWords } from '@/service'
+
+
+console.log('popup.js loading...')
+
+export const useSource = () => {
+  const [data, setData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true)
+    queryWords().then(res => {
+      setData(res);
+    }).finally(() => setLoading(false))
+  }, [])
+
+  return [data, loading] as const
+}
+
+const App = () => {
+  const [dataSource, loading] = useSource()
+
+  return (
+    <div className="popup">
+      <List dataSource={dataSource} loading={loading} />
+    </div>
+  );
+}
+
+interface IList {
+  dataSource: any[];
+  loading: boolean;
+}
+
+const List = (props: IList) => {
+  const { dataSource, loading } = props
+
+  const bodyContent = dataSource.map(
+    (datum, idx) => {
+      return (
+        <tr key={idx}>
+          <td>{idx}</td>
+          <td>{datum.from}</td>
+          <td>{datum.to}</td>
+          <td>{datum.count}</td>
+        </tr>
+      )
+    }
+  )
+
+  if (loading) {
+    return (
+      <div>
+        loading...
+      </div>
+    )
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Index</th>
+          <th>From</th>
+          <th>To</th>
+          <th>Times</th>
+        </tr>
+      </thead>
+      <tbody>
+        {bodyContent}
+      </tbody>
+    </table>
+  )
+}
+
+const root = document.getElementById('root') as HTMLElement
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -18,6 +19,11 @@ export default defineConfig({
           return '[name].js'
         },
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     }
   }
 })
