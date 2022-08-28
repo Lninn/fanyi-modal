5228495a65ee34108d74e47d0f71369dfd129aea
Lninn <thl384458870@outlook.com>
2022-08-28 14:20:23 +0800

    update

---
+++ src/service/SaveServer.ts
@@ -0,0 +1,26 @@
+
+
+export interface IServer {
+
+  getList: () => any[]
+  add: () => void
+  remove: () => void
+
+}
+
+export class Server implements IServer {
+  getList() {
+    return []
+  }
+
+  add() {
+    return
+  }
+
+  remove() {
+    return
+  }
+  
+}
+
+export default {}
