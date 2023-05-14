641b5d3f17b10ea0e36552c368c6b218adcb4497
Lninn <thl384458870@outlook.com>
2023-05-15 00:41:05 +0800

    ui update

--- src/dialog.tsx
+++ src/dialog.tsx
@@ -1,11 +1,14 @@
 import './dialog.less'
 
-import { useEffect, useRef, useState } from 'react'
+import { CSSProperties, SVGProps, useEffect, useRef, useState } from 'react'
 import { createPortal } from "react-dom"
 import { createRoot } from 'react-dom/client'
 
 // ref set variable
 // https://css-tricks.com/updating-a-css-variable-with-javascript/
+
+// icon
+// ref https://icones.js.org/collection/svg-spinners
 
 const DIALOG_WIDTH = 300
 const VIEW_WIDTH = document.documentElement.clientWidth
@@ -31,6 +34,10 @@ function Dialog() {
   selectedTextRef.current = selectedText
   
   const [visible, setVisible] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const [resultText, setResultText] = useState('')
+
+  const [isToggle, setIsToggle] = useState(false)
   
   const openRef = useRef(visible)
   openRef.current = visible
@@ -72,6 +79,10 @@ function Dialog() {
       const selection = window.getSelection()
       if (!selection) return
 
+      console.log(
+        is_closest_dialog(ev.target as any)
+      )
+
       if (is_closest_dialog(ev.target as any)) {
         return
       }
@@ -92,18 +103,57 @@ function Dialog() {
     }
   }, [])
 
-  const stl: React.CSSProperties = {
+  useEffect(() => {
+    if (!selectedText) return
+
+    setLoading(true)
+    query(selectedText).then(res => {
+      setResultText(res.text)
+    }).finally(() => setLoading(false))
+  }, [selectedText])
+
+  const dialogStyle: React.CSSProperties = {
     opacity: visible ? 1 : 0,
     width: DIALOG_WIDTH,
   }
 
+  const resultTextStl: CSSProperties = loading ? {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100
+  } : {}
+
+  const handleToggle = (ev: MouseEvent) => {
+    setIsToggle(!isToggle)
+
+    ev.stopPropagation()
+  }
+
   const dom = (
     <div className="wrap" ref={wrapRef}>
-      <div className='dialog' style={stl}>
-        <h4>Translate</h4>
-        <div>{selectedText}</div>
+      <div className='dialog' style={dialogStyle}>
+        <div className='title select_title'>
+          原文
+          <div
+            onClick={handleToggle}
+            style={{ fontSize: 24 }}
+          >
+            {
+              isToggle ? <BxToggleLeft /> : <BxToggleRight />
+            }
+          </div>
+        </div>
+        <div className='text'>{selectedText}</div>
         <hr />
-        <div>译文文本</div>
+        <div className='title'>译文</div>
+        <div className='text' style={resultTextStl}>
+          {
+            loading ? (
+              <SvgSpinnersEclipse />
+            ) : resultText
+          }
+        </div>
       </div>
     </div>
   )
@@ -157,4 +207,30 @@ export function create_dialog() {
     )
   )
 
+}
+
+function query(text: string) {
+  return new Promise<any>((resolve) => {
+    setTimeout(() => {
+      resolve({ text})
+    }, 1000)
+  })
+}
+
+function SvgSpinnersEclipse(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M2,12A11.2,11.2,0,0,1,13,1.05C12.67,1,12.34,1,12,1a11,11,0,0,0,0,22c.34,0,.67,0,1-.05C6,23,2,17.74,2,12Z"><animateTransform attributeName="transform" dur="0.6s" repeatCount="indefinite" type="rotate" values="0 12 12;360 12 12"></animateTransform></path></svg>
+  )
+}
+
+function BxToggleLeft(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M8 9c-1.628 0-3 1.372-3 3s1.372 3 3 3s3-1.372 3-3s-1.372-3-3-3z"></path><path fill="currentColor" d="M16 6H8c-3.3 0-5.989 2.689-6 6v.016A6.01 6.01 0 0 0 8 18h8a6.01 6.01 0 0 0 6-5.994V12c-.009-3.309-2.699-6-6-6zm0 10H8a4.006 4.006 0 0 1-4-3.99C4.004 9.799 5.798 8 8 8h8c2.202 0 3.996 1.799 4 4.006A4.007 4.007 0 0 1 16 16zm4-3.984l.443-.004l.557.004h-1z"></path></svg>
+  )
+}
+
+function BxToggleRight(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M16 9c-1.628 0-3 1.372-3 3s1.372 3 3 3s3-1.372 3-3s-1.372-3-3-3z"></path><path fill="currentColor" d="M16 6H8c-3.296 0-5.982 2.682-6 5.986v.042A6.01 6.01 0 0 0 8 18h8c3.309 0 6-2.691 6-6s-2.691-6-6-6zm0 10H8a4.006 4.006 0 0 1-4-3.99C4.004 9.799 5.798 8 8 8h8c2.206 0 4 1.794 4 4s-1.794 4-4 4z"></path></svg>
+  )
 }
